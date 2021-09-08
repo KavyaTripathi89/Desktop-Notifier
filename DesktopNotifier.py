ba7a4d4d0858241b4ef03f290fb9b2ee32a7cb3d
Kavya Tripathi <60308043+KavyaTripathi89@users.noreply.github.com>
2021-09-08 23:35:22 +0530

    Update DesktopNotifier.py

--- DesktopNotifier.py
+++ DesktopNotifier.py
@@ -6,7 +6,7 @@ if __name__=="__main__":
     while True:
         notification.notify(
             title = "Please drink water",
-            message = random.choice(["It boosts skin, health and beauty", "Makes your mind stronger", "Increases intelligence"]), 
+            message = random.choice(["It boosts skin, health and beauty", "Makes your mind stronger", "Increases intelligence","More Water, Good health"]), 
             app_icon = None,
             timeout = 10
 
